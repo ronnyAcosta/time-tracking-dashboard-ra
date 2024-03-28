e0b5d729b83da3af4e82bca2a2976fdc4542cbd3
Ronny Acosta <racosta0254@gmail.com>
2024-03-28 03:07:34 -0400

    Removed Unnecessary IDs

--- main.js
+++ main.js
@@ -3,20 +3,6 @@ import data from './data.json'
 const daily = document.querySelector('#daily');
 const weekly = document.querySelector('#weekly');
 const monthly = document.querySelector('#monthly');
-/*
-const workTime = document.querySelector('#work-time');
-const prevWorkTime = document.querySelector('#prev-work-time');
-const playTime = document.querySelector('#play-time');
-const prevPlayTime = document.querySelector('#prev-play-time');
-const studyTime = document.querySelector('#study-time');
-const prevStudyTime = document.querySelector('#prev-study-time');
-const exerciseTime = document.querySelector('#exercise-time');
-const prevExerciseTime = document.querySelector('#prev-exercise-time');
-const socialTime = document.querySelector('#social-time');
-const prevSocialTime = document.querySelector('#prev-social-time');
-const selfCareTime = document.querySelector('#self-care-time');
-const prevSelfCareTime = document.querySelector('#prev-self-care-time');
-*/
 
 const time = document.getElementsByClassName('time');
 const prevTime = document.getElementsByClassName('prev-time');
